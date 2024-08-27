fafbd41c88955b8f6c88f82e49a8722fa209a40b
Eymen Ünay <eymenunay@outlook.com>
2024-08-27 11:13:53 +0300

    Add sinc shifted by j test for periodicity by 2048

--- tests/SincTest.cpp
+++ tests/SincTest.cpp
@@ -1,6 +1,6 @@
-#include <gtest/gtest.h>
-#include <cmath>
 #include "comparison.h"
+#include <cmath>
+#include <gtest/gtest.h>
 
 TEST(SincTest, ZeroInput) {
     EXPECT_DOUBLE_EQ(scaled_sinc(0.0), 1.0);
@@ -15,15 +15,29 @@ TEST(ScaledSincTest, EvenFunction) {
     EXPECT_NEAR(scaled_sinc_j(x, j), scaled_sinc_j(-x, -j), 1e-6);
 }
 
-TEST(ScaledSincTest, PeriodicityCheck) {
-    double x = 1.0 / (2048.0);  // This should correspond to the first zero of sinc
-    EXPECT_NEAR(scaled_sinc(x), 0.0, 1e-6);
+TEST(ScaledSincJTest, Periodicity) {
+    double x = 1.5; // An arbitrary value
+    int j = 10;     // An arbitrary shift
 
-    int j = 0;
-    EXPECT_NEAR(scaled_sinc_j(x, j), 0.0, 1e-6);
+    // Test periodicity: f(x, j) should equal f(x + 2048, j)
+    EXPECT_NEAR(scaled_sinc_j(x, j), scaled_sinc_j(x + 2048, j), 1e-6);
+
+    // Test periodicity: f(x, j) should equal f(x - 2048, j)
+    EXPECT_NEAR(scaled_sinc_j(x, j), scaled_sinc_j(x - 2048, j), 1e-6);
+
+    // Test the specific case you mentioned: f(1, j) should equal f(-2047, j)
+    EXPECT_NEAR(scaled_sinc_j(1, j), scaled_sinc_j(-2047, j), 1e-6);
+
+    // Test with different j values
+    for (int test_j = -5; test_j <= 5; ++test_j) {
+        EXPECT_NEAR(scaled_sinc_j(x, test_j), scaled_sinc_j(x + 2048, test_j),
+                    1e-6);
+        EXPECT_NEAR(scaled_sinc_j(x, test_j), scaled_sinc_j(x - 2048, test_j),
+                    1e-6);
+    }
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
