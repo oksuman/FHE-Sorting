cdb4fadb20885d86c3c730cd7125bdf6fe3b7463
Eymen Ünay <eymenunay@outlook.com>
2024-09-02 13:31:56 +0300

    Add base sort class and propagate compile time evaluation

--- app/sort_algo.h
+++ app/sort_algo.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "comparison.h"
 #include "encryption.h"
 #include "openfhe.h"
 #include <iostream>
@@ -9,22 +10,186 @@
 
 using namespace lbcrypto;
 
-class DirectSort {
+// Abstract base class for sorting algorithms
+template <int N> // Array size
+class SortBase {
+
+  public:
+    SortBase() {}
+
+    virtual ~SortBase() = default;
+
+    virtual Ciphertext<DCRTPoly>
+    sort(const Ciphertext<DCRTPoly> &input_array) = 0;
+
+    // Common methods that can be used by all sorting algorithms
+    constexpr size_t getArraySize() const { return N; }
+};
+
+template <int N> class DirectSort : public SortBase<N> {
   private:
     CryptoContext<DCRTPoly> m_cc;
     PublicKey<DCRTPoly> m_PublicKey;
+    Comparison comp;
 
   public:
     std::shared_ptr<Encryption> m_enc;
     // Constructor
     DirectSort(CryptoContext<DCRTPoly> cc, PublicKey<DCRTPoly> publicKey,
                std::shared_ptr<Encryption> enc)
-        : m_cc(cc), m_PublicKey(publicKey), m_enc(enc) {}
+        : m_cc(cc), m_PublicKey(publicKey), comp(enc), m_enc(enc) {}
 
-    // Main ranking function
-    Ciphertext<DCRTPoly> constructRank(const Ciphertext<DCRTPoly> &input_array);
     Ciphertext<DCRTPoly>
-    constructRankv2(const Ciphertext<DCRTPoly> &input_array);
+    constructRank(const Ciphertext<DCRTPoly> &input_array) {
+
+        std::vector<double> Zero(N, 0.0);
+        Plaintext ptxZero = m_cc->MakeCKKSPackedPlaintext(Zero);
+        auto ctxRank = m_cc->Encrypt(m_PublicKey, ptxZero);
+        const auto inputOver255 =
+            m_cc->EvalMult(input_array, (double)1.0 / 255);
+        PRINT_PT(m_enc, inputOver255);
+
+#pragma omp parallel for
+        for (int i = 1; i < N; i++) {
+            std::cout << "Rotation index " << -i << "\n";
+            auto rotated = m_cc->EvalRotate(inputOver255, -i);
+            auto compResult = comp.compare(m_cc, inputOver255, rotated);
+// TODO remove critical section for performance and instead add results later
+#pragma omp critical
+            { m_cc->EvalAddInPlace(ctxRank, compResult); }
+        }
+
+        return ctxRank;
+    }
+
     Ciphertext<DCRTPoly>
-    constructRankv3(const Ciphertext<DCRTPoly> &input_array);
+    rotationIndexCheck(const Ciphertext<DCRTPoly> &Index_minus_Rank,
+                       const Ciphertext<DCRTPoly> &input_array) {
+        // int N = input_array->GetSlots();
+        constexpr uint32_t sincPolyDegree = 7721;
+        // Create a zero plaintext
+        std::vector<double> Zero(N, 0.0);
+        const Plaintext ptx_Zero = m_cc->MakeCKKSPackedPlaintext(Zero);
+
+        // Initialize output array
+        auto output_array = m_cc->Encrypt(m_PublicKey, ptx_Zero);
+
+        const int block_size = 32;
+        const int num_blocks = N / block_size;
+
+#pragma omp parallel for
+        for (int i = 0; i < num_blocks; i++) {
+            auto intermediate = m_cc->Encrypt(m_PublicKey, ptx_Zero);
+            PRINT_PT(m_enc, intermediate);
+
+            for (int j = 0; j < block_size; j++) {
+                int rotation = i * block_size + j;
+                if (i == 0 && j == 0)
+                    continue; // Skip the first rotation (no rotation needed)
+
+                // Compute the sinc interpolation for this rotation
+                auto rotIndex = m_cc->EvalChebyshevFunction(
+                    [rotation](double x) {
+                        return Sinc<N>::scaled_sinc_j(x, rotation);
+                    },
+                    Index_minus_Rank, -1, 1, sincPolyDegree);
+
+                // Apply the rotation mask to the input array
+                auto masked_input =
+                    m_cc->EvalMultAndRelinearize(rotIndex, input_array);
+
+                // Rotate the masked input and add to intermediate result
+                int effective_rotation = (rotation % N) - block_size;
+                m_cc->EvalAddInPlace(
+                    intermediate,
+                    m_cc->EvalRotate(masked_input, effective_rotation));
+            }
+
+            // Apply final rotation to the intermediate result
+            int block_rotation = ((i - (num_blocks - 1)) * block_size + N) % N;
+            intermediate = m_cc->EvalRotate(intermediate, block_rotation);
+
+// Add the intermediate result to the output array
+#pragma omp critical
+            { m_cc->EvalAddInPlace(output_array, intermediate); }
+        }
+
+        return output_array;
+    }
+
+    Ciphertext<DCRTPoly>
+    rotationIndexCheckv2(const Ciphertext<DCRTPoly> &Index_minus_Rank,
+                         const Ciphertext<DCRTPoly> &input_array) {
+        // int N = input_array->GetSlots();
+        constexpr int sincPolyDegree = 7701;
+
+        // TODO This is done to prevent rotation in the loop. However this means
+        // more sequential execution.
+        //  OpenFHE EvalRotate -> EvalAtIndex actually checks for 0 rotation and
+        //  clones the ct back. Check if it is better to keep it in the loop for
+        //  parallelisation.
+        auto rotIndex = m_cc->EvalChebyshevFunction(
+            Sinc<N>::scaled_sinc, Index_minus_Rank, -1, 1, sincPolyDegree);
+        PRINT_PT(m_enc, rotIndex);
+        auto output_array = m_cc->EvalMultAndRelinearize(rotIndex, input_array);
+        PRINT_PT(m_enc, output_array);
+
+        // TODO share precompute values with rank construction.
+        //  Create a zero plaintext
+        std::vector<double> Zero(N, 0.0);
+        Plaintext ptx_Zero = m_cc->MakeCKKSPackedPlaintext(Zero);
+
+        for (int i = 1; i < N; i++) {
+            // Compute the sinc interpolation for this rotation
+            auto rotIndex = m_cc->EvalChebyshevFunction(
+                [i](double x) { return Sinc<N>::scaled_sinc_j(x, i); },
+                Index_minus_Rank, -1, 1, 7701);
+            PRINT_PT(m_enc, rotIndex);
+
+            // Apply the rotation mask to the input array
+            auto masked_input =
+                m_cc->EvalMultAndRelinearize(rotIndex, input_array);
+
+            // Rotate the masked input
+            auto rotated = m_cc->EvalRotate(masked_input, i);
+
+            // Add to the output array
+            m_cc->EvalAddInPlace(output_array, rotated);
+
+            // Debug: print intermediate results
+            std::cout << "Rotation " << i << ": \n";
+            PRINT_PT(m_enc, rotated);
+        }
+
+        return output_array;
+    }
+
+    Ciphertext<DCRTPoly>
+    sort(const Ciphertext<DCRTPoly> &input_array) override {
+
+        auto ctx_Rank = constructRank(input_array);
+        PRINT_PT(m_enc, ctx_Rank);
+
+        // auto input_over_255 = m_cc->EvalMult(input_array, (double)1.0 / 255);
+        // auto b = m_cc->EvalRotate(input_over_255, -N/2);
+        // auto comp1 = comp.compare(m_cc, input_over_255, b);
+        // m_cc->EvalAddInPlace(ctx_Rank, comp1);
+        // PRINT_PT(m_enc, ctx_Rank);
+
+        // Make a plaintext index array [1, 2, 3, ...]
+        std::vector<double> Index(N);
+        std::iota(Index.begin(), Index.end(), 0);
+        Plaintext ptx_Index = m_cc->MakeCKKSPackedPlaintext(Index);
+        std::cout << ptx_Index << "\n";
+
+        // Evaluate index - rank, which denotes the rotation index to be sorted
+        auto Index_minus_Rank = m_cc->EvalSub(ptx_Index, ctx_Rank);
+        PRINT_PT(m_enc, Index_minus_Rank);
+
+        m_cc->EvalMultInPlace(Index_minus_Rank, 1.0 / N);
+        PRINT_PT(m_enc, Index_minus_Rank);
+
+        auto output_array = rotationIndexCheckv2(Index_minus_Rank, input_array);
+        return output_array;
+    }
 };
