3f4579c91a36a65f86580fb5f16aa030f9354e68
Eymen Ünay <eymenunay@outlook.com>
2024-09-02 13:31:56 +0300

    Add base sort class and propagate compile time evaluation

--- app/sort_algo.cpp
+++ app/sort_algo.cpp
@@ -6,23 +6,3 @@
 #include <cassert>
 #include <memory>
 #include <omp.h>
-
-Ciphertext<DCRTPoly>
-DirectSort::constructRankv3(const Ciphertext<DCRTPoly> &input_array) {
-
-    Comparison comp(m_enc);
-    int N = input_array->GetSlots();
-    std::vector<double> Zero(N, 0.0);
-    Plaintext ptxZero = m_cc->MakeCKKSPackedPlaintext(Zero);
-    auto ctxRank = m_cc->Encrypt(m_PublicKey, ptxZero);
-    const auto inputOver255 = m_cc->EvalMult(input_array, (double)1.0 / 255);
-
-#pragma omp parallel for
-    for (int i = 1; i < N; i++) {
-        auto rotated = m_cc->EvalRotate(inputOver255, -i);
-        auto compResult = comp.compare(m_cc, inputOver255, rotated);
-        m_cc->EvalAddInPlace(ctxRank, compResult);
-    }
-
-    return ctxRank;
-}
