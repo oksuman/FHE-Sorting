92006d06f3ead9970fa34f1efe0101a3ceeb8dfd
Eymen Ünay <eymenunay@outlook.com>
2024-09-02 13:28:35 +0300

    Add compile time size N for Sinc with template

--- app/comparison.cpp
+++ app/comparison.cpp
@@ -1,32 +1,5 @@
 #include "comparison.h"
 
-double scaled_sinc(double x) {
-    if (std::abs(x) < 1e-10) {
-        return 1.0;
-    } else {
-        return std::sin(M_PI * 2048 * x) / (M_PI * 2048 * x);
-    }
-}
-
-double scaled_sinc_j(double x, int j) {
-    const double epsilon = 1e-10;
-    const double factor = 2048 * M_PI;
-
-    double denominator1 = factor * x - j * M_PI;
-    double denominator2 = denominator1 + factor;
-
-    double term1 = (std::abs(denominator1) < epsilon)
-                       ? 1.0
-                       : std::sin(denominator1) / denominator1;
-    double term2 = (std::abs(denominator2) < epsilon)
-                       ? 1.0
-                       : std::sin(denominator2) / denominator2;
-
-    double result = term1 + term2;
-
-    return result;
-}
-
 Ciphertext<lbcrypto::DCRTPoly> g_n(Ciphertext<lbcrypto::DCRTPoly> x,
                                    CryptoContext<DCRTPoly> cc) {
     std::vector<double> coeffs = {0.0, 1.112086941473206858077560355014,
--- app/comparison.h
+++ app/comparison.h
@@ -6,10 +6,37 @@
 #include <cmath>
 #include <memory>
 
-double scaled_sinc(double x);
-double scaled_sinc_j(double x, int j);
-
 using namespace lbcrypto;
+
+template <int N> struct Sinc {
+
+    static double scaled_sinc(double x) {
+        if (std::abs(x) < 1e-10) {
+            return 1.0;
+        } else {
+            // Periodic by the size of the array
+            return std::sin(M_PI * N * x) / (M_PI * N * x);
+        }
+    }
+    static double scaled_sinc_j(double x, int j) {
+        constexpr double epsilon = 1e-10;
+        constexpr double factor = N * M_PI;
+
+        double denominator1 = factor * x - j * M_PI;
+        double denominator2 = denominator1 + factor;
+
+        double term1 = (std::abs(denominator1) < epsilon)
+                           ? 1.0
+                           : std::sin(denominator1) / denominator1;
+        double term2 = (std::abs(denominator2) < epsilon)
+                           ? 1.0
+                           : std::sin(denominator2) / denominator2;
+
+        double result = term1 + term2;
+
+        return result;
+    }
+};
 
 class Comparison {
   private:
